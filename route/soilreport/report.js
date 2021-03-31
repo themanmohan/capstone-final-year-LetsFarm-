cc88788ec47ee29fcd9bd46c906bc39ac87bd3de
unknown <gitmail223344@gmail.com>
2021-03-31 20:53:56 +0530

    improvement of soil detail and report

--- route/soilreport/report.js
+++ route/soilreport/report.js
@@ -16,7 +16,7 @@ const {
 //@desc     add report to  snapshot
 //@route     GET/soildetails/detail_id/report/new
 //@access    private(admin only)
-router.get('/report/new',isLoggedIn,authorize('admin'), (req, res) => {
+router.get('/new',isLoggedIn,authorize('admin'), (req, res) => {
      SoilDetail.findById(req.params.detail_id, (err, detail) => {
           if (err) {
 
@@ -34,7 +34,7 @@ router.get('/report/new',isLoggedIn,authorize('admin'), (req, res) => {
 //@desc     add report to  snapshot
 //@route     POST/soildetails/detail_id/report
 //@access    private(admin only)
-router.post("/report", isLoggedIn, authorize('admin'), (req, res) => {
+router.post("/", isLoggedIn, authorize('admin'), (req, res) => {
      SoilDetail.findById(req.params.detail_id, (err, detail) => {
           if (err) {
                console.log(err)
@@ -57,4 +57,36 @@ router.post("/report", isLoggedIn, authorize('admin'), (req, res) => {
 })
 
 
+//@desc     update report to  snapshot
+//@route     PUT/soildetails/detail_id/report/report_id/edit
+//@access    private(admin only)
+router.get('/:report_id/edit',isLoggedIn,authorize('admin'),(req,res)=>{
+
+     SoilDetail.findById(req.params.detail_id,(err,detail)=>{
+        if(err){
+             console.log(err)
+        }
+
+        SoilReport.findById(req.params.report_id,(err,report)=>{
+           if (err) {
+                console.log(err)
+           }
+           res.render('soilreport/editreport',{detail,report})
+        })
+     })
+  
+})
+
+
+//@desc     update report to  snapshot
+//@route     PUT/soildetails/detail_id/report/report_id/edit
+//@access    private(admin only)
+router.put('/:report_id', isLoggedIn, authorize('admin'), (req, res) => {
+   SoilReport.findByIdAndUpdate(req.params.report_id,req.body.report,(err,updatereport)=>{
+     if(err){
+          console.log(err)
+     }
+     res.redirect("/soildetails/admindashboard")
+   })
+})
 module.exports = router
